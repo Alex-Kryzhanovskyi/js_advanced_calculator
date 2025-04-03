3e06a9ee4d8154c3be60b51c6459ff11aa6eab9e
Alex Kryzhanovskyi <kryzhanoskyi.oleksandr@gmail.com>
2025-04-03 17:59:43 +0300

    calc

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -3,8 +3,31 @@
 /**
  * @return {object}
  */
+
 function makeCalculator() {
-  // write code here
+  const systemObj = {
+    result: 0,
+    add: (value) => value,
+    subtract: (value) => -value,
+    multiply: (value, currentResult) => value * currentResult,
+    divide: (value, current) => (value === 0 ? current : current / value),
+    operate(callback, inputNum) {
+      if (callback === this.multiply || callback === this.divide) {
+        this.result = callback(inputNum, this.result);
+      } else {
+        this.result = this.result + callback(inputNum);
+      }
+
+      return this;
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return systemObj;
 }
 
 module.exports = makeCalculator;
